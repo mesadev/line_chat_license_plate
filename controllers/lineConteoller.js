14187f0002ac4bbd1994fc1a04977dfb8143de9e
Mesa <mesa.na.dev@gmail.com>
2020-01-09 15:24:54 +0700

    .

--- controllers/lineConteoller.js
+++ controllers/lineConteoller.js
@@ -1,7 +1,18 @@
 module.exports.test = async (req, res) => {
-    return await res.send('Hello test')
+    let a = test()
+    return await res.send(a)
 }
 
 module.exports.webhook = async (req, res) => {
-    return await res.send('Hello webhook')
+    try {
+        let reply_token = req.body.events[0].replyToken
+        let a = test()
+    } catch (error) {
+        console.log(error)
+    }
+    return await res.sendStatus(200)
+}
+
+function test(){
+    return "ddddd"
 }
